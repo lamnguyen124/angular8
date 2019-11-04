d35dca03f07b147fdd6797314a06534cf06c8704
Lam <nguyen.h.lam12@gmail.com>
2019-11-04 14:56:02 -0600

    playing with creating and editing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
