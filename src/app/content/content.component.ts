fe55128f5babdba885702368809939c654eb47dc
Lam <nguyen.h.lam12@gmail.com>
2019-11-05 16:44:08 -0600

    dynamic style

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -4,18 +4,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ContentComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverId = 10;
+  serverStatus = 'offline';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
    }
 
   ngOnInit() {
@@ -23,10 +31,19 @@ export class ContentComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
