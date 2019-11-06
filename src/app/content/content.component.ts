c6f4e1d0f1cd9b030de57f81289746e0dd4fae19
Lam <nguyen.h.lam12@gmail.com>
2019-11-06 11:28:09 -0600

    added dates instead of numbers

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -47,6 +47,7 @@ export class ContentComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
